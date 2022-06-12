1f8a3ca9328760f0bf92d1687394e43da6a0fb29
ElnurZeynalov <tu74449ce@code.edu.az>
2022-06-12 20:15:15 +0400

    Task

--- TestImtahan/TestImtahan/Areas/Manage/Controllers/SliderController.cs
+++ TestImtahan/TestImtahan/Areas/Manage/Controllers/SliderController.cs
@@ -5,6 +5,7 @@ using System.Collections.Generic;
 using System.IO;
 using System.IO.Pipelines;
 using System.Threading.Tasks;
+using System.Linq;
 using TestImtahan.DAL;
 using TestImtahan.Models;
 
@@ -34,14 +35,55 @@ namespace TestImtahan.Areas.Manage.Controllers
         public async Task<IActionResult> Create(ClientsComments comments)
         {
             string fileName = comments.FullName + comments.Image.FileName;
-            string path = Path.Combine();
             using (FileStream fs = new FileStream(Path.Combine(_env.WebRootPath, "assets/image", fileName), FileMode.Create))
             {
                 comments.Image.CopyTo(fs);
-            };
+            }
             comments.ImageUrl = fileName;
             await _context.Comments.AddAsync(comments);
             await _context.SaveChangesAsync();
+            return RedirectToAction("Index");
+        }
+        public IActionResult Edit(int id)
+        {
+            ClientsComments comment = _context.Comments.Find(id);
+            if(comment == null) return NotFound();
+            return View(comment);
+        }
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(ClientsComments newComment)
+        {
+            if (newComment == null) return NotFound();
+            ClientsComments oldComment = _context.Comments.Find(newComment.Id);
+            string deletePath = Path.Combine(_env.WebRootPath, "assets", "image" + oldComment.ImageUrl);
+            if (System.IO.File.Exists(deletePath))
+            {
+                System.IO.File.Delete(deletePath);
+            }
+            string fileName = newComment.FullName + newComment.Image.FileName;
+            using (FileStream fs = new FileStream(Path.Combine(_env.WebRootPath, "assets/image", fileName), FileMode.Create))
+            {
+                newComment.Image.CopyTo(fs);
+            }
+            oldComment.FullName = newComment.FullName;
+            oldComment.Comment = newComment.Comment;
+            oldComment.ImageUrl = fileName;
+            oldComment.Position = newComment.Position;
+            await _context.SaveChangesAsync();
+            return RedirectToAction("Index");
+        }
+        public IActionResult Delete(int id)
+        {
+            ClientsComments deletedComment = _context.Comments.Find(id);
+            if (deletedComment == null) return NotFound();
+            _context.Comments.Remove(deletedComment);
+            _context.SaveChanges();
+            string deletePath = Path.Combine(_env.WebRootPath, "assets", "image" + deletedComment.ImageUrl);
+            if (System.IO.File.Exists(deletePath))
+            {
+                System.IO.File.Delete(deletePath);
+            }
             return RedirectToAction("Index");
         }
     }
